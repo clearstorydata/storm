df91b60789dc0255276725a24531e0a7c7f0312d
Mikhail Bautin <mbautin@gmail.com>
2013-03-22 17:32:04 -0700

    Avoid uploading to clojars

--- bin/to_maven.sh
+++ bin/to_maven.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+set -e -u -o pipefail
+
 RELEASE=`head -1 project.clj | awk '{print $3}' | sed -e 's/\"//' | sed -e 's/\"//'`
 
 rm -rf classes
-rm *jar
-rm *xml
+rm -f *jar
+rm -f *xml
 lein jar
 lein pom
-scp storm*jar pom.xml clojars@clojars.org:
+#scp storm*jar pom.xml clojars@clojars.org:
 
-rm *jar
+rm -f *jar
 rm -rf classes
-rm conf/log4j.properties
+rm -f conf/log4j.properties
 lein jar
 mv pom.xml old-pom.xml
 sed 's/artifactId\>storm/artifactId\>storm-lib/g' old-pom.xml > pom.xml
 mv storm-$RELEASE.jar storm-lib-$RELEASE.jar
-scp storm*jar pom.xml clojars@clojars.org:
-rm *xml
-rm *jar
+#scp storm*jar pom.xml clojars@clojars.org:
+rm -f *xml
+rm -f *jar
 git checkout conf/log4j.properties
+
