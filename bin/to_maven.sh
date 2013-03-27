8057b525db86b70cafa80882b726ee2302acc15c
mbautin <mbautin@gmail.com>
2013-03-26 22:27:29 -0700

    Save generated pom and jar files to the target directory

--- bin/to_maven.sh
+++ bin/to_maven.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-set -e -u -o pipefail
+time (
 
-RELEASE=`head -1 project.clj | awk '{print $3}' | sed -e 's/\"//' | sed -e 's/\"//'`
+set -e -u -o pipefail -x
+cd $(dirname $0)/..
+mkdir -p target
 
+version=`head -1 project.clj | awk '{print $3}' | sed -e 's/\"//' | sed -e 's/\"//'`
+
+# Build storm
 rm -rf classes
 rm -f *jar
 rm -f *xml
+rm -f pom.xml
+
 lein jar
 lein pom
-#scp storm*jar pom.xml clojars@clojars.org:
 
+mv -f storm-$version.jar target
+cp -f pom.xml target/storm-$version.pom
+
+# Build storm-lib
 rm -f *jar
 rm -rf classes
 rm -f conf/log4j.properties
+
 lein jar
 mv pom.xml old-pom.xml
 sed 's/artifactId\>storm/artifactId\>storm-lib/g' old-pom.xml > pom.xml
-mv storm-$RELEASE.jar storm-lib-$RELEASE.jar
-#scp storm*jar pom.xml clojars@clojars.org:
+
+mv -f storm-$version.jar target/storm-lib-$version.jar
+mv -f pom.xml target/storm-lib-$version.pom
+
 rm -f *xml
 rm -f *jar
 git checkout conf/log4j.properties
+
+)
 
